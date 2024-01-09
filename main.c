cbcdb00ea4f3033c1fd750f43407a6329c1d18cf
Robert D. French <robert@robertdfrench.me>
2024-01-08 23:57:19 -0500

    Introduce C in first chunk of code.

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 int main() {
     // First chunk of code
     int a = 1;
+    int c = 3;
 
     // Second chunk of code
     int b = 2;
 
-    printf("sum = %d\n", a + b);
+    printf("sum = %d\n", a + b + c);
 
     return 0;
 }
